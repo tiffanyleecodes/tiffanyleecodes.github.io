132d7386359ef54ca04da064ebe1ef461c875e2f
tiffanyleecodes <b0023060@gmail.com>
2023-03-28 10:46:05 +1200

    Change project link of mobile

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ open.addEventListener("click", function () {
 close.addEventListener("click", function () {
   menu.style.right = "-100%";
 });
+
+const side = document.querySelectorAll(".Side");
+const hover = document.querySelectorAll(".hover");
+const link = document.querySelectorAll(".link");
+
+for (let i = 0; i < side.length; i++) {
+  side[i].addEventListener("click", function () {
+    if (hover[i].style.opacity === "1") {
+      return;
+    }
+    for (let j = 0; j < hover.length; j++) {
+      hover[j].style.opacity = "0";
+      link[j].style.display = "none";
+    }
+    hover[i].style.opacity = "1";
+    link[i].style.display = "flex";
+  });
+}
